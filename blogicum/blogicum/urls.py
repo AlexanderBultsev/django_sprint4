bc3aef6b35ccfd6e9a49f4a0636a1eaa6553bb73
Alexander Bultsev <bultsevalexander@yandex.ru>
2025-02-21 20:29:56 +0300

    v1.4

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -28,13 +28,14 @@ handler500 = 'pages.views.internal_server_error'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('auth/registration/',
-         CreateView.as_view(
-             template_name='registration/registration_form.html',
-             form_class=UserCreationForm,
-             success_url=reverse_lazy('blog:index')
-         ),
-         name='registration'
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index')
+        ),
+        name='registration'
     ),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
